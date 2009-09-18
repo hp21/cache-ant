160a78ab5d2bc21fc852427b69c99556833ceb3b
HP <HP@localhost>
2009-09-18 15:46:50 +0200

    Issue 3:
    Cache connection is now closed in filnally block.

--- src/com/googlecode/cacheant/CacheCompileTask.java
+++ src/com/googlecode/cacheant/CacheCompileTask.java
@@ -13,207 +13,217 @@ import com.intersys.objects.CacheException;
 import com.intersys.objects.Database;
 
 /**
- * @author u292148
+ * @author HP
  * 
  */
 public class CacheCompileTask extends Task {
 
-	private String url;
+  private String url;
 
-	private String classes;
+  private String classes;
 
-	private String itemList;
+  private String itemList;
 
-	private String packageStr;
+  private String packageStr;
 
-	private String project;
+  private String project;
 
-	private boolean all;
+  private boolean all;
 
-	private String user;
-	private String password;
+  private String user;
+  private String password;
 
-	class ParamBuilder {
+  class ParamBuilder {
 
-		ParamBuilder() {
-		}
+    ParamBuilder() {
+    }
 
-		public CParams buildParams() {
-			CParams cparams = new CParams();
-			cparams.setUrl(url);
+    public CParams buildParams() {
+      CParams cparams = new CParams();
+      cparams.setUrl(url);
 
-			cparams.setcClazz("%SYSTEM.OBJ");
+      cparams.setcClazz("%SYSTEM.OBJ");
 
-			if (null != classes) {
-				cparams.setCmethod("Compile");
-				cparams.setParams(new Object[] { classes });
-			} else if (null != getProjectName()) {
-				cparams.setCmethod("CompileProject");
-				cparams.setParams(new Object[] { getProjectName() });
-			} else if (null != itemList) {
-				cparams.setCmethod("CompileList");
-				cparams.setParams(new Object[] { itemList });
-			} else if (null != packageStr) {
-				cparams.setCmethod("CompilePackage");
-				cparams.setParams(new Object[] { packageStr });
-			} else if (all) {
-				cparams.setCmethod("CompileAll");
-				cparams.setParams(new Object[] {});
-			}
+      if (null != classes) {
+        cparams.setCmethod("Compile");
+        cparams.setParams(new Object[] { classes });
+      } else if (null != getProjectName()) {
+        cparams.setCmethod("CompileProject");
+        cparams.setParams(new Object[] { getProjectName() });
+      } else if (null != itemList) {
+        cparams.setCmethod("CompileList");
+        cparams.setParams(new Object[] { itemList });
+      } else if (null != packageStr) {
+        cparams.setCmethod("CompilePackage");
+        cparams.setParams(new Object[] { packageStr });
+      } else if (all) {
+        cparams.setCmethod("CompileAll");
+        cparams.setParams(new Object[] {});
+      }
 
-			// all compile task returns %Status
-			cparams.setRetrunStatus(true);
+      // all compile task returns %Status
+      cparams.setRetrunStatus(true);
 
-			return cparams;
-		}
-	}
+      return cparams;
+    }
+  }
 
-	public CacheCompileTask() {
-		user = "_SYSTEM";
-		password = "sys";
-	}
+  public CacheCompileTask() {
+    user = "_SYSTEM";
+    password = "sys";
+  }
 
-	@Override
-	public void execute() throws BuildException {
-		log("execute -- start", Project.MSG_DEBUG);
-		log("Compiling " + getMyDescription(), Project.MSG_INFO);
-		checkParams();
+  @Override
+  public void execute() throws BuildException {
+    log("execute -- start", Project.MSG_DEBUG);
+    log("Compiling " + getMyDescription(), Project.MSG_INFO);
+    checkParams();
 
-		try {
-			Database db = CacheDatabase.getDatabase(url/* , user, password */);
-			CacheHelper helper = new CacheHelper(db);
+    Database db = null;
 
-			CParams cparams = new ParamBuilder().buildParams();
+    try {
+      db = CacheDatabase.getDatabase(url/* , user, password */);
+      CacheHelper helper = new CacheHelper(db);
 
-			boolean status = helper.callMethod(cparams);
+      CParams cparams = new ParamBuilder().buildParams();
 
-			if (!status) {
-				log("Error in cache compile", Project.MSG_ERR);
-				throw new BuildException("Error in cache compile");
-			}
+      boolean status = helper.callMethod(cparams);
 
-		} catch (CacheException e) {
-			throw new BuildException(e);
-		}
+      if (!status) {
+        log("Error in cache compile", Project.MSG_ERR);
+        throw new BuildException("Error in cache compile");
+      }
 
-		super.execute();
-		log("execute -- end", Project.MSG_DEBUG);
-	}
+    } catch (CacheException e) {
+      throw new BuildException(e);
+    } finally {
+      if (null != db) {
+        try {
+          db.close();
+        } catch (CacheException e) {
+          log("Can not close Cache connection", Project.MSG_ERR);
+        }
+      }
+    }
 
-	private void checkParams() throws BuildException {
+    super.execute();
+    log("execute -- end", Project.MSG_DEBUG);
+  }
 
-		if (null == url) {
-			throw new BuildException("Cache url must be specified");
-		}
+  private void checkParams() throws BuildException {
 
-		int counter = 0;
+    if (null == url) {
+      throw new BuildException("Cache url must be specified");
+    }
 
-		if (null != classes) {
-			counter++;
-		}
+    int counter = 0;
 
-		if (null != itemList) {
-			counter++;
-		}
+    if (null != classes) {
+      counter++;
+    }
 
-		if (null != packageStr) {
-			counter++;
-		}
+    if (null != itemList) {
+      counter++;
+    }
 
-		if (null != project) {
-			counter++;
-		}
+    if (null != packageStr) {
+      counter++;
+    }
 
-		if (all) {
-			counter++;
-		}
+    if (null != project) {
+      counter++;
+    }
 
-		if (counter > 1) {
-			log(
-			    "Too many compile parameters give. Only one can be specified from [project,package,itemList,classes,all]",
-			    Project.MSG_ERR);
-			throw new BuildException(
-			    "Too many compile parameters give. Only one can be specified from [project,package,itemList,classes,all]");
-		}
+    if (all) {
+      counter++;
+    }
 
-		if (0 == counter) {
-			log("No compile parameters specified. Please specify at least one from [project,package,itemList,classes,all] ");
-			throw new BuildException(
-			    "No compile parameters specified. Please specify at least one from [project,package,itemList,classes] ");
-		}
+    if (counter > 1) {
+      log(
+          "Too many compile parameters give. Only one can be specified from [project,package,itemList,classes,all]",
+          Project.MSG_ERR);
+      throw new BuildException(
+      "Too many compile parameters give. Only one can be specified from [project,package,itemList,classes,all]");
+    }
 
-	}
+    if (0 == counter) {
+      log("No compile parameters specified. Please specify at least one from [project,package,itemList,classes,all] ");
+      throw new BuildException(
+      "No compile parameters specified. Please specify at least one from [project,package,itemList,classes] ");
+    }
 
-	public String getMyDescription() {
-		return new StringBuilder().append(null != classes ? "classlist: " + classes : "").append(
-		    null != itemList ? "itemList: " + itemList : "").append(
-		    null != packageStr ? "package=" + packageStr : "").append(
-		    null != project ? "project=" + project : "").append(all ? "all classes in namespace" : "")
-		    .toString();
-	}
+  }
 
-	public String getUrl() {
-		return url;
-	}
+  public String getMyDescription() {
+    return new StringBuilder().append(null != classes ? "classlist: " + classes : "").append(
+        null != itemList ? "itemList: " + itemList : "").append(
+            null != packageStr ? "package=" + packageStr : "").append(
+                null != project ? "project=" + project : "").append(all ? "all classes in namespace" : "")
+                .toString();
+  }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+  public String getUrl() {
+    return url;
+  }
 
-	public String getClasses() {
-		return classes;
-	}
+  public void setUrl(String url) {
+    this.url = url;
+  }
 
-	public void setClasses(String classes) {
-		this.classes = classes;
-	}
+  public String getClasses() {
+    return classes;
+  }
 
-	public String getItemList() {
-		return itemList;
-	}
+  public void setClasses(String classes) {
+    this.classes = classes;
+  }
 
-	public void setItemList(String itemList) {
-		this.itemList = itemList;
-	}
+  public String getItemList() {
+    return itemList;
+  }
 
-	public String getPackage() {
-		return packageStr;
-	}
+  public void setItemList(String itemList) {
+    this.itemList = itemList;
+  }
 
-	public void setPackage(String packageStr) {
-		this.packageStr = packageStr;
-	}
+  public String getPackage() {
+    return packageStr;
+  }
 
-	public String getProjectName() {
-		return project;
-	}
+  public void setPackage(String packageStr) {
+    this.packageStr = packageStr;
+  }
 
-	public void setProjectName(String projectName) {
-		this.project = projectName;
-	}
+  public String getProjectName() {
+    return project;
+  }
 
-	public String getUser() {
-		return user;
-	}
+  public void setProjectName(String projectName) {
+    this.project = projectName;
+  }
 
-	public void setUser(String user) {
-		this.user = user;
-	}
+  public String getUser() {
+    return user;
+  }
 
-	public String getPassword() {
-		return password;
-	}
+  public void setUser(String user) {
+    this.user = user;
+  }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+  public String getPassword() {
+    return password;
+  }
 
-	public boolean isAll() {
-		return all;
-	}
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
-	public void setAll(boolean all) {
-		this.all = all;
-	}
+  public boolean isAll() {
+    return all;
+  }
+
+  public void setAll(boolean all) {
+    this.all = all;
+  }
 
 }
